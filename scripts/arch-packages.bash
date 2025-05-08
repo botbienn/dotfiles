6411bb547355dabc368befd120ff46c214b61da9
botbien <vubac2004@gmail.com>
2025-05-08 17:04:15 +0700

    update fonts, waybar and some stuffs

--- scripts/arch-packages.bash
+++ scripts/arch-packages.bash
@@ -1,2 +1,4 @@
 sudo pacman -Syu
-sudo pacman -S tmux git githubcli neovim grim slurp stow lazygit fzf npm fcitx5-bamboo fcitx5-configtool
+sudo pacman -S tmux git githubcli neovim grim slurp stow 
+sudo pacman -S lazygit fzf npm fcitx5-bamboo fcitx5-configtool waybar
+sudo pacman -S otf-font-awesome
