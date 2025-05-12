cfe3a041d67a31d4a259e0d9a166b9e521d288c5
botbien <vubac2004@gmail.com>
2025-05-12 20:11:01 +0700

    update default packages

--- scripts/arch-packages.bash
+++ scripts/arch-packages.bash
@@ -1,4 +1,5 @@
 sudo pacman -Syu
 sudo pacman -S tmux git githubcli neovim grim slurp stow 
 sudo pacman -S lazygit fzf npm fcitx5-bamboo fcitx5-configtool waybar
-sudo pacman -S otf-font-awesome
+sudo pacman -S otf-font-awesome xorg-xinput zsh-autosuggestions zsh-autocomplete
+sudo pacman -S zsh-syntax-highlighting
